ce8710190308f0209ee7bbaddca1fdc256ce4af6
KyuDev <quentin.schilling@gmx.de>
2022-01-27 19:37:00 +0100

    fixessss

--- get_html.py
+++ get_html.py
@@ -1,4 +1,8 @@
-import urllib
+from email import header
+import urllib.request
 
 url_n = input("url: ")
-urllib.urlretrieve(url_n, "safe.html")
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36')]
+urllib.request.install_opener(opener)
+urllib.request.urlretrieve(url_n, "safe.html")
